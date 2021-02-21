bcab383c256bad07ffcf1cfec88ad83a100b2ef5
seblaz <sebastian.blazquez96@gmail.com>
2021-02-21 15:19:30 -0300

    Change token generator

--- src/infra/servicios/GeneradorToken.ts
+++ src/infra/servicios/GeneradorToken.ts
@@ -1,9 +1,9 @@
 import IGeneradorToken from "../../domain/servidores/servicios/GeneradorToken";
-import tokenGenerator from "uuid-apikey"
+import crypto from "crypto"
 
 
 export class GeneradorToken implements IGeneradorToken {
     generarToken(): string {
-        return tokenGenerator.create().apiKey;
+        return crypto.randomBytes(24).toString('hex');
     }
 }
